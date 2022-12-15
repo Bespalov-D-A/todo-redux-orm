b6c4325e8696cf55aa148180a265be407d55d2b1
Denis <Googlonator@gmail.com>
2022-12-15 16:36:54 +0400

    feature U

--- src/components/Logo/LogoJJ.js
+++ src/components/Logo/LogoJJ.js
@@ -1,11 +1,11 @@
 import s from './Logo.module.css'
 import img from './../../img/ico.png'
 
-const Logo = () => {
+const LogoJJ = () => {
   return <div className={s.logo}>
     <img src={img} alt="" />
     <h2><span>TODO</span>BASE</h2>
     </div>
 }
 
-export default Logo
+export default LogoJJ
